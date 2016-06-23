171677fbc4635cceadb5340d8fd9469346080960
GCotumaccio <cotumaccio@gmail.com>
2016-06-23 15:45:11 +0200

    First Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var textFieldNumbers: UITextField!
+    
+    // Variables
+    var number1 = 0
+    var number2 = 0
+    var operationType = ""
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        textFieldNumbers.text = sender.currentTitle
+    }
+    
+    @IBAction func operation(sender: UIButton) {
+        operationType = sender.currentTitle!
+        number1 = Int(textFieldNumbers.text!)!
+    }
+    
+    @IBAction func equalOperation(sender: UIButton) {
+        
+        number2 = Int(textFieldNumbers.text!)!
+        
+        var result = 0
+        
+        if operationType == "+" {
+            result = number1 + number2
+            // print("Equal: \(number1) \(number2)")
+        } else if operationType == "-" {
+            result = number1 - number2
+        } else if operationType == "x" {
+            result = number1 * number2
+        }
+        
+        textFieldNumbers.text = result.description
+    }
+    
+    @IBAction func resetValues(sender: UIButton) {
+        number1 = 0
+        number2 = 0
+        operationType = ""
+        textFieldNumbers.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
